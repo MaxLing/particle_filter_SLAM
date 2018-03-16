116750cdec5a34a68ab240a83d5bedafcd3a0d3a
Max LING <wudaoling7@gmail.com>
2018-03-15 21:06:29 -0400

    framework for mapping

--- SLAM.py
+++ SLAM.py
@@ -16,16 +16,31 @@ def main():
     # init
     Map, Particles = init_SLAM()
 
-    for t in range(100):
-        # Mapping
-        lidar_scan = lidar_data[t]['scan']
-        good_range = np.logical_and(lidar_scan>0.1, lidar_scan<30) # lidar spec
-        lidar_xyz = polar2cart(lidar_scan[good_range], lidar_angles[good_range]) # in lidar cart coordinate 3*n
+    Trajectory = []
+    for lidar_idx in range(10):
+        Pose = Particles['poses'][Particles['best_idx']] # TODO: now only the best particle is used
 
-        print(0)
+        # Mapping
+        # extract lidar scan in good range and transform to lidar's cart coordinate
+        lidar_scan = lidar_data[lidar_idx]['scan']
+        good_range = np.logical_and(lidar_scan>0.1, lidar_scan<30) # lidar spec
+        lidar_hit = polar2cart(lidar_scan[good_range], lidar_angles[good_range]) # 3*n
+
+        # find closest joint data
+        joint_idx = np.argmin(np.abs(joint_data['ts']-lidar_data[lidar_idx]['t']))
+        joint_angles = joint_data['head_angles'][:,joint_idx]
+
+        # transform hit from lidar to world coordinate, remove ground hitting
+        world_hit = lidar2world(lidar_hit, joint_angles, Pose)
+        not_floor = world_hit[2,:]>0.1
+        world_hit = world_hit[:,not_floor]
+
+        # update map according to hit
+        update_map(world_hit[:2], Pose[:2], Map)
+
         # Localization
 
-
+        print('this is for debug')
 
 def data_preprocess(joint_dir, lidar_dir):
     joint_data = ld.get_joint(joint_dir)
@@ -44,11 +59,16 @@ def init_SLAM():
     Map['res'] = 20 # cells / m
     Map['size'] = 50 # m
     Map['map'] = np.zeros((Map['res']*Map['size'], Map['res']*Map['size'])) # log odds
+    belief = 0.9 # prob of lidar hit if the grid is occupied
+    Map['occ_d'] = np.log(belief/(1-belief))
+    Map['free_d'] = np.log((1-belief)/belief)*.5
     # TODO: set a bound for log odds
 
     Particles = {}
     Particles['nums'] = 10
     Particles['weights'] = np.ones(Particles['nums']) / Particles['nums']
+    Particles['poses'] = np.zeros((3, Particles['nums']))
+    Particles['best_idx'] = 0
 
     return Map, Particles
 
--- SLAM_functions.py
+++ SLAM_functions.py
@@ -1,10 +1,64 @@
 import numpy as np
+import cv2
 
 def polar2cart(scan, angles):
-    # good_scan = lidar_scan[good_range]
-    # good_angles = lidar_angles[good_range]
-
     x = scan * np.cos(angles)
     y = scan * np.sin(angles)
     z = np.zeros(len(scan))
     return np.vstack((x, y, z))
+
+def lidar2world(lidar_hit, joint_angles, pose):
+    neck_angle = joint_angles[0] # yaw wrt body frame
+    head_angle = joint_angles[1] # pitch wrt body frame
+
+    # lidar wrt head
+    z_hl = 0.15
+    H_hl = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,z_hl],[0,0,0,1]]) # no rotation
+
+    # head wrt body
+    z_bh = 0.33
+    T_bh = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,z_bh],[0,0,0,1]])
+    Rz = np.array([[np.cos(neck_angle), -np.sin(neck_angle), 0, 0],
+                    [np.sin(neck_angle), np.cos(neck_angle), 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+    Ry = np.array([[np.cos(head_angle), 0, np.sin(head_angle), 0],
+                   [0, 1, 0, 0],
+                   [-np.sin(head_angle), 0, np.cos(head_angle), 0],
+                   [0, 0, 0, 1]])
+    R_bh = np.dot(Rz,Ry)
+    H_bh = np.dot(T_bh, R_bh)
+
+    # body wrt world
+    x_gb = pose[0]
+    y_gb = pose[1]
+    z_gb = 0.93
+    yaw_gb = pose[2]
+    R_gb = np.array([[np.cos(yaw_gb), -np.sin(yaw_gb), 0, 0],
+                    [np.sin(yaw_gb), np.cos(yaw_gb), 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+    T_gb = np.array([[1,0,0,x_gb],[0,1,0,y_gb],[0,0,1,z_gb],[0,0,0,1]])
+    H_gb = np.dot(T_gb, R_gb)
+
+    # lidar wrt world
+    H_gl = H_gb.dot(H_bh).dot(H_hl)
+    lidar_hit = np.vstack((lidar_hit,np.ones((1,lidar_hit.shape[1])))) # 4*n
+    world_hit = np.dot(H_gl, lidar_hit)
+
+    return world_hit[:3,:]
+
+def update_map(hit, pose, Map):
+    # transform hit to grid, and check boundary
+    grid = np.uint((hit+Map['size']/2)*Map['res']) # 2*n
+    in_bound = np.logical_and(np.abs(grid[0])<=Map['size']*Map['res'], np.abs(grid[1])<=Map['size']*Map['res'])
+    grid = grid[:,in_bound]
+
+    # update log odds for occupied grid
+    Map['map'][grid[0], grid[1]] += Map['occ_d']
+
+    # update log odss for free grid, using contours to mask region between pose and hit
+    mask = np.zeros(Map['map'].shape)
+    contour = np.hstack((pose.reshape(-1,1),hit))
+    cv2.drawContours(image=mask, contours = [contour], contourIdx = -1, color = Map['free_d'], thickness=-1)
+    Map['map'] += mask
